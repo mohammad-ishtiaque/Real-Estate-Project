d57e01beb045f895f2c5bce85e25882fb599cea1
mohammad-ishtiaque <mohammad.ishtiaque833@gmail.com>
2025-02-13 18:57:40 +0600

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+import { type } from './../../node_modules/raw-body/index.d';
+
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
