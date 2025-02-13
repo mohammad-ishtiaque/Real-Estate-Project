1b5a8870f7a0c8d9d5d3fb100205cb67ad55ef91
mohammad-ishtiaque <mohammad.ishtiaque833@gmail.com>
2025-02-14 00:14:14 +0600

    complete signup page functionality

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -9,7 +9,7 @@ signup =  async(req, res, next) => {
     const newUser = new User({ username, email, password:hashedPassword });
     try {
         await newUser.save();
-        res.status(201).send('User created successfully');
+        res.status(201).json('User created successfully');
     }   catch (err) {
         next(err);
     }
